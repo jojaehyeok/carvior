265e7bf41471382a36e2b512f2cae87300e91fdc
JoJaehyeok <jjhstc2285@naver.com>
2026-08-15 00:46:21 +0900

    fix: 브랜드 추정 정규식 보강(BMW 모델코드, 벤츠 디젤/하이브리드 접미사, KGM 필랑트)
    420i/320d(BMW), E220d(벤츠)처럼 숫자 뒤 연료/파워트레인 접미사가 붙은 모델코드가 안 잡히던 문제 수정

--- lib/carBrand.ts
+++ lib/carBrand.ts
@@ -10,17 +10,18 @@ export interface BrandRule {
 export const BRAND_RULES: BrandRule[] = [
   { pattern: /테슬라|tesla/i, slug: 'tesla', label: '테슬라' },
   // "벤츠" 표기가 없어도 C220처럼 벤츠 모델코드(C/E/S클래스, GLA~GLS, CLA, CLS)만 적혀있는 경우도 매칭
-  { pattern: /벤츠|벤즈|mercedes|메르세데스|\b(?:[CES]\s?\d{3}|GL[ABCES]|CLA|CLS)\b/i, slug: 'mercedes-benz', label: '벤츠' },
+  { pattern: /벤츠|벤즈|mercedes|메르세데스|\b(?:[CES]\s?\d{3}(?:d|e|h)?|GL[ABCES]|CLA|CLS)\b/i, slug: 'mercedes-benz', label: '벤츠' },
   // 브랜드명 없이 모델명만 적힌 경우도 추정: 국내 매물 특성상 자주 그렇게 씀
   { pattern: /현대|hyundai|아반떼|소나타|그랜저|투싼|싼타페|팰리세이드|코나|캐스퍼|베뉴|아이오닉|스타렉스|포터|넥쏘|벨로스터/i, slug: 'hyundai', label: '현대' },
   { pattern: /기아|\bkia\b|\bk[3-9]\b|모닝|레이|셀토스|스포티지|쏘렌토|카니발|모하비|스팅어|니로|\bev[369]\b/i, slug: 'kia', label: '기아' },
-  { pattern: /\bbmw\b|\b[1-8]\s?시리즈|\bx[1-7]\b|\bm[2-8]\b|\bi[347]\b|\bix\b/i, slug: 'bmw', label: 'BMW' },
+  // 320d, 420i, 730Li, M340i처럼 시리즈명 없이 모델코드(숫자+연료/파워트레인 접미사)만 적힌 경우도 매칭
+  { pattern: /\bbmw\b|\b[1-8]\s?시리즈|\bx[1-7]\b|\bm[2-8]\b|\bi[347]\b|\bix\b|\bm?[1-8]\d{2}(?:i|d|e|li|xi|si)\b/i, slug: 'bmw', label: 'BMW' },
   { pattern: /아우디|audi|\ba[1-8]\b|\bq[2-8]\b|\btt\b|\br8\b|e-?tron/i, slug: 'audi', label: '아우디' },
   { pattern: /제네시스|genesis|\bg[789]0\b|\bgv(60|70|80)\b/i, slug: 'genesis', label: '제네시스' },
   { pattern: /쉐보레|쉐비|chevrolet|스파크|말리부|트랙스|트레일블레이저|이쿼녹스|콜로라도|카마로/i, slug: 'chevrolet', label: '쉐보레(GM대우)' },
   { pattern: /르노|renault|\bsm[3-6]\b|\bqm[3-6]\b|\bxm3\b|클리오|아르카나|트위지/i, slug: 'renault', label: '르노코리아(삼성)' },
   // 쌍용차 → KGM(KG모빌리티)로 사명 변경돼서 신형 문서엔 KGM으로 적히는 경우가 많음
-  { pattern: /쌍용|ssangyong|kg\s?모빌리티|\bkgm\b|렉스턴|티볼리|코란도|무쏘|액티언|카이런|체어맨|토레스/i, slug: 'ssangyong', label: 'KGM(쌍용)' },
+  { pattern: /쌍용|ssangyong|kg\s?모빌리티|\bkgm\b|렉스턴|티볼리|코란도|무쏘|액티언|카이런|체어맨|토레스|필랑트/i, slug: 'ssangyong', label: 'KGM(쌍용)' },
   { pattern: /폭스바겐|volkswagen|\bvw\b|골프|제타|파사트|티구안|투아렉|아테온|폴로/i, slug: 'volkswagen', label: '폭스바겐' },
   { pattern: /토요타|도요타|toyota|캠리|코롤라|프리우스|라브4|시에나|아발론|하이랜더/i, slug: 'toyota', label: '토요타' },
   { pattern: /렉서스|lexus|\bes\d{3}\b|\bis\d{3}\b|\brx\d{3}\b|\bnx\d{3}\b|\bls\d{3}\b|\block?x\d{3}\b/i, slug: 'lexus', label: '렉서스' },
